b5d6c958b1a883b3c66d8da13a70eae6776976bf
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-20 19:18:29 +0800

    BMI
    BMI

---
+++ app/src/main/java/com/courtney/bmikotlin/BmiRunner.kt
@@ -0,0 +1,12 @@
+package com.courtney.bmikotlin
+
+
+fun main() {
+
+    val weight = 77.5f
+    var height = 1.7f
+    height = 1.75f
+    val bmi = weight / (height * height)
+    println("Your BMI is: $bmi")
+
+}
--- app/src/main/java/com/courtney/bmikotlin/MainActivity.kt
+++ app/src/main/java/com/courtney/bmikotlin/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.courtney.bmikotlin
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_bmi.setOnClickListener {
+            val weight = edt_weight.text.toString().toFloat()
+            val height = edt_height.text.toString().toFloat()
+            val bmi = weight / (height * height)
+            Log.d(TAG, "bmi: $bmi")
+            Toast.makeText(this, "bmi: $bmi", Toast.LENGTH_LONG).show()
+            AlertDialog.Builder(this)
+                .setTitle("BMI")
+                .setMessage("BMI: $bmi")
+                .setPositiveButton("OK", null)
+                .show()
+        }
     }
 }
